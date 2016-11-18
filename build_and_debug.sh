f8ef9fec4d966d018a26e2af1409a83f83abcb7a
Jon New <jonnew@gmail.com>
2016-11-18 18:00:17 -0500

    Getting rid of build_and_debug.h which I'm not using.

--- build_and_debug.sh
+++
@@ -1,8 +0,0 @@
-rm output/*
-i686-elf-as gdt.s -o output/gdt.o;
-i686-elf-as boot.s -o output/boot.o;
-i686-elf-gcc -g -c kernel.c  -o output/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
-i686-elf-gcc -g -c utils.c  -o output/utils.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
-i686-elf-gcc -g -c terminal.c  -o output/terminal.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
-i686-elf-gcc -g -T linker.ld -o output/myos.bin -ffreestanding -O2 -nostdlib output/terminal.o output/utils.o output/boot.o output/gdt.o output/kernel.o -lgcc;
-qemu-system-i386 -s -S -kernel output/myos.bin;
