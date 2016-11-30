bacf4132e9887259ab1b083186c550fb2483da02
Jon New <jonnew@gmail.com>
2016-11-30 14:10:14 -0500

    Moving interrupt stuff out of gdt.s

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,6 @@
 rm output/*
 i686-elf-as gdt.s -o output/gdt.o;
+i686-elf-as interrupt_asm.s -o output/interrupt_asm.o;
 i686-elf-as boot.s -o output/boot.o;
 i686-elf-gcc -c kernel.c  -o output/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
 i686-elf-gcc -c interrupts.c  -o output/interrupts.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
@@ -10,7 +11,7 @@ i686-elf-gcc -c debug.c  -o output/debug.o -std=gnu99 -ffreestanding -O2 -Wall -
 i686-elf-gcc -c utils.c  -o output/utils.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
 i686-elf-gcc -c terminal.c  -o output/terminal.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
 i686-elf-gcc -c notepad.c  -o output/notepad.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra;
-i686-elf-gcc -T linker.ld -o output/myos.bin -ffreestanding -O2 -nostdlib output/notepad.o output/keyboard.o output/interrupts.o output/descriptor_tables.o output/asm.o output/debug.o output/terminal.o output/utils.o output/boot.o output/gdt.o output/kernel.o -lgcc;
+i686-elf-gcc -T linker.ld -o output/myos.bin -ffreestanding -O2 -nostdlib output/notepad.o output/keyboard.o output/interrupts.o output/descriptor_tables.o output/asm.o output/debug.o output/terminal.o output/utils.o output/boot.o output/interrupt_asm.o output/gdt.o output/kernel.o -lgcc;
 qemu-system-i386 -serial file:serial.log  -kernel output/myos.bin; #-serial file:serial.log
 
 
